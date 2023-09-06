f3a23c55c30edbf3072336d31f043ec07c3f0629
Toño <caaz160782@gmail.com>
2023-09-05 19:43:43 -0600

    url dinamica next

---
+++ pages/blog/[url].js
@@ -0,0 +1,51 @@
+import { useRouter } from "next/router"
+import Image from "next/image"
+import Layout from "../../components/layout"
+import {formatearFecha} from '../../utils/helpers'
+import styles from '../../styles/blog.module.css' 
+
+const PostInfo = ({blog}) => {
+    const router = useRouter();
+    const {contenido,imagen,titulo,publishedAt }=blog[0].attributes
+  return (
+    <Layout
+    title={`Blog ${titulo}` }    
+    >
+    <article className={`${styles.post} ${styles['mt-3']}`}>
+    <Image src={imagen.data.attributes.formats.medium.url} width={600} height={400} alt={`Imagen blog ${titulo}`}/>
+     <div className={styles.contenido}>
+       <h3>{titulo}</h3>
+       <p className={styles.fecha}>{formatearFecha(publishedAt)}</p>
+       <p className={styles.texto}>{contenido}</p>    
+     </div> 
+   </article>
+    </Layout>
+  )
+}
+
+export default PostInfo
+
+export async function getStaticPaths(){
+  const respuesta = await fetch(`${process.env.API_URL}/posts`)
+  const {data } = await respuesta.json();
+  
+  const paths= data.map(blog =>({
+          params:{
+            url:blog.attributes.url
+          }
+  }))
+  return{
+    paths,
+    fallback:false
+  }
+}
+
+export async function getStaticProps({params: {url}}){
+  const respuesta = await fetch(`${process.env.API_URL}/posts?filters[url]=${url}&populate=imagen`)
+  const {data: blog} = await respuesta.json();
+  return{
+   props:{
+    blog
+   }
+  }
+}
